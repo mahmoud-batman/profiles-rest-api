79e821281c59f84b1acac9b6765088c11a7d1b01
mahmoud-batman <mahmoud148043@gmail.com>
2019-12-20 22:40:51 +0000

    Django admin model

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,3 +1,45 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin)
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            return ValueError("must have email")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+"""
+    AbstractUser: 
+    Use this option if you are happy with the existing fields on the User model and just want to 
+    remove the username field.
+    AbstractBaseUser: 
+    Use this option if you want to start from scratch by creating your own,completely new User model.
+"""
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True)
+    name = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return "@{}".format(self.name)
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -117,6 +117,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+""" custom user model """
+AUTH_USER_MODEL = "profile_api.UserProfile"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
